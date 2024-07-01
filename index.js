d3f4b29cb519fb8ee2f7fdcfb554ee20c6aa3b35
shaonkislo2 <shaon6340@gmail.com>
2024-06-30 21:32:14 -0700

    get services done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -8,9 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-// name- navanaUser
-// pass- uAYguUrv1m2n5hKC
 
 console.log(process.env.DB_PASS);
 
@@ -29,6 +27,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("navanaCar").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -36,7 +43,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
