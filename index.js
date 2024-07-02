44810aaa2d3d8a99b0754e561d8f1e106c8d072e
shaonkislo2 <shaon6340@gmail.com>
2024-07-01 17:59:58 -0700

    Booking Items Deleted Done

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
@@ -52,8 +52,12 @@ async function run() {
     // bookings
 
     app.get("/bookings", async (req, res) => {
-      console.log(req.query);
-      const result = await bookingCollection.find().toArray();
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
       res.send(result);
     });
 
@@ -61,6 +65,14 @@ async function run() {
       const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    // Delete
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
